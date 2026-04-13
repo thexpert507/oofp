82abc75c6cc34f596069242899c7e5f499352245
Adriel Avila <adrielavila@MacBook-Pro-de-Adriel.local>
2026-04-13 09:35:51 -0500

    fix: bind run method in PendingUpdate constructor for correct context

--- packages/focal/lib/builder/pending-update.ts
+++ packages/focal/lib/builder/pending-update.ts
@@ -1,7 +1,9 @@
 import { Fn } from "@oofp/core/function";
 
 export class PendingUpdate<S, A = S> {
-	constructor(private readonly updater: Fn<S, A>) {}
+	constructor(private readonly updater: Fn<S, A>) {
+		this.run = this.run.bind(this);
+	}
 
 	run(s: S): A {
 		return this.updater(s);
